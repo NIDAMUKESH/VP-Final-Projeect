36a57a4c4e40470f385ec1a81d68af898b896b26
NIDAMUKESH <nidamukesh@gmail.com>
2023-05-20 21:15:17 +0500

    1st STEP(Design)

---
+++ winSchoolMS/winSchoolMS/Form1.Designer.cs
@@ -0,0 +1,388 @@
+﻿namespace winSchoolMS
+{
+    partial class frmFeesDetail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtStudentID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFatherName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLibraryFees = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtExaminationFees = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtClass = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtDiscount = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtFine = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtTutionfees = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtPayDate = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gvFeesDetail = new System.Windows.Forms.DataGridView();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtDueDate = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.txtTotalAmount = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gvFeesDetail)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(28, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Student ID";
+            // 
+            // txtStudentID
+            // 
+            this.txtStudentID.Location = new System.Drawing.Point(95, 71);
+            this.txtStudentID.Name = "txtStudentID";
+            this.txtStudentID.Size = new System.Drawing.Size(146, 20);
+            this.txtStudentID.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 4;
+            // 
+            // txtFatherName
+            // 
+            this.txtFatherName.Location = new System.Drawing.Point(623, 70);
+            this.txtFatherName.Name = "txtFatherName";
+            this.txtFatherName.Size = new System.Drawing.Size(146, 20);
+            this.txtFatherName.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(540, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Father Name";
+            // 
+            // txtLibraryFees
+            // 
+            this.txtLibraryFees.Location = new System.Drawing.Point(623, 128);
+            this.txtLibraryFees.Name = "txtLibraryFees";
+            this.txtLibraryFees.Size = new System.Drawing.Size(146, 20);
+            this.txtLibraryFees.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(554, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Library fees";
+            // 
+            // txtStudentName
+            // 
+            this.txtStudentName.Location = new System.Drawing.Point(357, 71);
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new System.Drawing.Size(146, 20);
+            this.txtStudentName.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(276, 74);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Student Name";
+            // 
+            // txtExaminationFees
+            // 
+            this.txtExaminationFees.Location = new System.Drawing.Point(357, 128);
+            this.txtExaminationFees.Name = "txtExaminationFees";
+            this.txtExaminationFees.Size = new System.Drawing.Size(146, 20);
+            this.txtExaminationFees.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(264, 132);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(87, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Examination fees";
+            // 
+            // txtClass
+            // 
+            this.txtClass.Location = new System.Drawing.Point(95, 131);
+            this.txtClass.Name = "txtClass";
+            this.txtClass.Size = new System.Drawing.Size(146, 20);
+            this.txtClass.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(41, 136);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Class";
+            // 
+            // txtDiscount
+            // 
+            this.txtDiscount.Location = new System.Drawing.Point(623, 201);
+            this.txtDiscount.Name = "txtDiscount";
+            this.txtDiscount.Size = new System.Drawing.Size(146, 20);
+            this.txtDiscount.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(557, 206);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(49, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Discount";
+            // 
+            // txtFine
+            // 
+            this.txtFine.Location = new System.Drawing.Point(357, 201);
+            this.txtFine.Name = "txtFine";
+            this.txtFine.Size = new System.Drawing.Size(146, 20);
+            this.txtFine.TabIndex = 19;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(294, 206);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(27, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Fine";
+            // 
+            // txtTutionfees
+            // 
+            this.txtTutionfees.Location = new System.Drawing.Point(95, 206);
+            this.txtTutionfees.Name = "txtTutionfees";
+            this.txtTutionfees.Size = new System.Drawing.Size(146, 20);
+            this.txtTutionfees.TabIndex = 23;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(24, 210);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(62, 13);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "Tuition fees";
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(199, 328);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 24;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(531, 328);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 25;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(357, 328);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 26;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // txtPayDate
+            // 
+            this.txtPayDate.Location = new System.Drawing.Point(357, 275);
+            this.txtPayDate.Name = "txtPayDate";
+            this.txtPayDate.Size = new System.Drawing.Size(146, 20);
+            this.txtPayDate.TabIndex = 28;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(286, 279);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "Pay Date";
+            // 
+            // gvFeesDetail
+            // 
+            this.gvFeesDetail.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.gvFeesDetail.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvFeesDetail.Location = new System.Drawing.Point(24, 374);
+            this.gvFeesDetail.Name = "gvFeesDetail";
+            this.gvFeesDetail.Size = new System.Drawing.Size(745, 181);
+            this.gvFeesDetail.TabIndex = 29;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(306, 9);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(163, 31);
+            this.label11.TabIndex = 30;
+            this.label11.Text = "Fees Detail";
+            // 
+            // txtDueDate
+            // 
+            this.txtDueDate.Location = new System.Drawing.Point(95, 278);
+            this.txtDueDate.Name = "txtDueDate";
+            this.txtDueDate.Size = new System.Drawing.Size(146, 20);
+            this.txtDueDate.TabIndex = 32;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(24, 282);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(51, 13);
+            this.label13.TabIndex = 31;
+            this.label13.Text = "Due date";
+            // 
+            // txtTotalAmount
+            // 
+            this.txtTotalAmount.Location = new System.Drawing.Point(623, 273);
+            this.txtTotalAmount.Name = "txtTotalAmount";
+            this.txtTotalAmount.Size = new System.Drawing.Size(146, 20);
+            this.txtTotalAmount.TabIndex = 34;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(552, 277);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(70, 13);
+            this.label14.TabIndex = 33;
+            this.label14.Text = "Total Amount";
+            // 
+            // frmFeesDetail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(790, 567);
+            this.Controls.Add(this.txtTotalAmount);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.txtDueDate);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.gvFeesDetail);
+            this.Controls.Add(this.txtPayDate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.txtTutionfees);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.txtFine);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtDiscount);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtClass);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtExaminationFees);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtStudentName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtLibraryFees);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtFatherName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtStudentID);
+            this.Controls.Add(this.label2);
+            this.Name = "frmFeesDetail";
+            this.Text = "Fees Detail";
+            ((System.ComponentModel.ISupportInitialize)(this.gvFeesDetail)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtStudentID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFatherName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLibraryFees;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtStudentName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtExaminationFees;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtClass;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtDiscount;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtFine;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtTutionfees;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtPayDate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView gvFeesDetail;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtDueDate;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox txtTotalAmount;
+        private System.Windows.Forms.Label label14;
+    }
+}
+
